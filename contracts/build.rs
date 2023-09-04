e9c362c4b8a04ca54fda68ae93c1b37207a8943c
Raffaele Ragni <raffaele.ragni@virtualminds.com>
2023-09-04 10:10:28 +0200

    setup contract generation from avro

---
+++ contracts/build.rs
@@ -0,0 +1,15 @@
+use std::{fs::File, io::Read};
+
+use rsgen_avro::{Generator, Source};
+
+fn main() {
+    let mut file = File::open("schema.json").unwrap();
+    let mut raw_schema = String::new();
+    file.read_to_string(&mut raw_schema).unwrap();
+
+    let source = Source::SchemaStr(&raw_schema);
+    let mut file = File::create("src/lib.rs").unwrap();
+
+    let g = Generator::new().unwrap();
+    g.gen(&source, &mut file).unwrap();
+}
