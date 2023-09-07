fc3f1158cd7891eb7adaf7b081a4b680da9ec9ad
Raffaele Ragni <raffaele.ragni@virtualminds.com>
2023-09-07 08:48:25 +0200

    cucumber skeletons

--- e2e/tests/e2e.rs
+++ e2e/tests/e2e.rs
@@ -1,6 +1,15 @@
-use cucumber::World;
+use cucumber::{given, then, when, World};
 
 mod setup;
+
+#[given(expr = "A started system")]
+async fn startup(_: &mut setup::World) {}
+
+#[when(expr = "Checking for health")]
+async fn health_check(_: &mut setup::World) {}
+
+#[then(expr = "Health check was OK")]
+async fn health_is_ok(_: &mut setup::World) {}
 
 #[tokio::main]
 async fn main() {
