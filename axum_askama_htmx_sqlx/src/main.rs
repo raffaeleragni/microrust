bb55ba007b706205c8a3aa6a6d023fa7c74c65d9
Raffaele Ragni <raffaele.ragni@gmail.com>
2024-01-03 23:13:29 +0100

    setup database bootstrap

--- axum_askama_htmx_sqlx/src/main.rs
+++ axum_askama_htmx_sqlx/src/main.rs
@@ -1,28 +1,46 @@
-mod ui;
 mod api;
+mod ui;
 
 use std::env;
 
 use anyhow::Result;
-use axum::{routing::get, Router};
+use axum::{routing::get, Extension, Router};
 use axum_prometheus::PrometheusMetricLayer;
+use sqlx::postgres::PgPoolOptions;
 use structured_logger::async_json::new_writer;
 use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    dotenv::dotenv()?;
     let listener = TcpListener::bind("0.0.0.0:3000").await?;
 
     logger();
     sentry();
 
     let mut app = Router::new();
+    app = database(app).await?;
     app = prometheus(app);
     app = ui::init(app);
     app = api::init(app);
 
+    log::info!("Starting server");
     axum::serve(listener, app).await?;
     Ok(())
+}
+
+async fn database(app: Router) -> Result<Router> {
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL environment variable not set");
+    let max_connections = env::var("DATABASE_MAX_CONNECTIONS")
+        .ok()
+        .and_then(|s| s.parse::<u32>().ok())
+        .unwrap_or(1);
+    let pool = PgPoolOptions::new()
+        .max_connections(max_connections)
+        .connect(&database_url)
+        .await?;
+    sqlx::migrate!().run(&pool).await?;
+    Ok(app.layer(Extension(pool)))
 }
 
 fn prometheus(app: Router) -> Router {
