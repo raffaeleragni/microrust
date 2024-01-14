6bd534ba8f0591132cc13c4c8bc4e38d6c9fa0ab
Raffaele Ragni <raffaele.ragni@gmail.com>
2024-01-14 23:13:55 +0100

    show list page

--- axum_askama_htmx_sqlx/src/ui.rs
+++ axum_askama_htmx_sqlx/src/ui.rs
@@ -3,16 +3,17 @@ use std::env;
 use anyhow::Result;
 use askama::Template;
 use askama_axum::IntoResponse;
-use axum::{http::header, routing::get, Extension, Router};
+use axum::{extract::Path, http::header, routing::get, Extension, Router};
 use rust_embed::RustEmbed;
-use sqlx::{query, Pool, Postgres};
+use sqlx::{query_as, Pool, Postgres};
 
 use crate::AppError;
 
 pub fn init(app: Router) -> Router {
     app.route("/", get(index))
         .route("/htmx.min.js", get(htmx))
-        .route("/sample", get(get_sample))
+        .route("/ui/samples", get(get_samples))
+        .route("/ui/sample", get(get_sample))
 }
 
 #[derive(Template)]
@@ -38,12 +39,38 @@ async fn htmx() -> impl IntoResponse {
         .into_response()
 }
 
+struct Sample {
+    id: String,
+    name: String,
+}
+
 #[derive(Template)]
-#[template(path = "view.html")]
-struct SampleView {}
+#[template(path = "samples.html")]
+struct SamplesView {
+    samples: Vec<Sample>,
+}
+
+#[derive(Template)]
+#[template(path = "sample.html")]
+struct SampleView {
+    sample: Sample,
+}
 
 #[axum::debug_handler]
-async fn get_sample(Extension(db): Extension<Pool<Postgres>>) -> Result<SampleView, AppError> {
-    let _sample = query!("select * from sample").fetch_one(&db).await?;
-    Ok(SampleView {})
+async fn get_samples(Extension(db): Extension<Pool<Postgres>>) -> Result<SamplesView, AppError> {
+    let samples = query_as!(Sample, "select * from sample")
+        .fetch_all(&db)
+        .await?;
+    Ok(SamplesView { samples })
+}
+
+#[axum::debug_handler]
+async fn get_sample(
+    Extension(db): Extension<Pool<Postgres>>,
+    Path(id): Path<String>,
+) -> Result<SampleView, AppError> {
+    let sample = query_as!(Sample, "select * from sample where id = $1", id)
+        .fetch_one(&db)
+        .await?;
+    Ok(SampleView { sample })
 }
