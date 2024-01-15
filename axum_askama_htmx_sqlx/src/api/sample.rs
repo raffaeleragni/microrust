e54eb70be6c55b01ee39eb50654985707eafd0a9
Raffaele Ragni <raffaele.ragni@gmail.com>
2024-01-15 23:19:39 +0100

    sample json return

--- axum_askama_htmx_sqlx/src/api/sample.rs
+++ axum_askama_htmx_sqlx/src/api/sample.rs
@@ -1,4 +1,5 @@
-use axum::routing::{get, Router};
+use axum::{routing::{get, Router}, Json};
+use serde::Serialize;
 
 use crate::errors::AppError;
 
@@ -6,7 +7,17 @@ pub fn init(app: Router) -> Router {
     app.route("/api/sample", get(get_samples))
 }
 
+#[derive(Serialize)]
+struct Sample {
+    id: String,
+    name: String,
+}
+
 #[axum::debug_handler]
-async fn get_samples() -> Result<String, AppError> {
-    Ok("[]".into())
+async fn get_samples() -> Result<Json<Vec<Sample>>, AppError> {
+    let result = vec![Sample {
+        id: "id".into(),
+        name: "name".into(),
+    }];
+    Ok(Json(result))
 }
