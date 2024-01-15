a951135d7a6bde191c06cdb3fc026c7355ce05d5
Raffaele Ragni <raffaele.ragni@gmail.com>
2024-01-15 23:44:52 +0100

    list api

--- axum_askama_htmx_sqlx/src/api/sample.rs
+++ axum_askama_htmx_sqlx/src/api/sample.rs
@@ -1,10 +1,14 @@
-use axum::{routing::{get, Router}, Json};
+use axum::{
+    routing::{get, Router},
+    Json, Extension,
+};
 use serde::Serialize;
+use sqlx::{Postgres, Pool, query_as};
 
 use crate::errors::AppError;
 
 pub fn init(app: Router) -> Router {
-    app.route("/api/sample", get(get_samples))
+    app.route("/api/sample", get(get_samples).post(new_sample))
 }
 
 #[derive(Serialize)]
@@ -14,10 +18,20 @@ struct Sample {
 }
 
 #[axum::debug_handler]
-async fn get_samples() -> Result<Json<Vec<Sample>>, AppError> {
-    let result = vec![Sample {
-        id: "id".into(),
-        name: "name".into(),
-    }];
+async fn get_samples(
+    Extension(db): Extension<Pool<Postgres>>,
+) -> Result<Json<Vec<Sample>>, AppError> {
+    let samples = query_as!(Sample, "select * from sample")
+        .fetch_all(&db)
+        .await?;
+    Ok(Json(samples))
+}
+
+#[axum::debug_handler]
+async fn new_sample() -> Result<Json<Sample>, AppError> {
+    let result = Sample {
+        id: "new".into(),
+        name: "newname".into(),
+    };
     Ok(Json(result))
 }
