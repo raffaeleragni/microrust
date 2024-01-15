2a53e4708c54c80bf8f3907f02618d708d7d3328
Raffaele Ragni <raffaele.ragni@gmail.com>
2024-01-15 20:09:13 +0100

    macro for statics

--- axum_askama_htmx_sqlx/src/ui/statics.rs
+++ axum_askama_htmx_sqlx/src/ui/statics.rs
@@ -1,18 +1,28 @@
 use askama_axum::IntoResponse;
-use axum::{http::header, Router, routing::get};
+use axum::{http::header, routing::get, Router};
 use rust_embed::RustEmbed;
-
-pub fn init(app: Router) -> Router {
-    app.route("/htmx.min.js", get(htmx))
-}
 
 #[derive(RustEmbed)]
 #[folder = "static"]
 struct Asset;
-async fn htmx() -> impl IntoResponse {
-    (
-        [(header::CONTENT_TYPE, "text/javascript")],
-        Asset::get("htmx.min.js").unwrap().data.to_vec(),
-    )
-        .into_response()
+
+pub fn init(app: Router) -> Router {
+    macro_rules! serve_static {
+        ($app:ident, $content:expr, $path:expr, $file:expr ) => {
+            let $app = $app.route(
+                $path,
+                get(|| async {
+                    (
+                        [(header::CONTENT_TYPE, $content)],
+                        Asset::get($file).unwrap().data.to_vec(),
+                    )
+                        .into_response()
+                }),
+            );
+        };
+    }
+
+    serve_static!(app, "text/javascript", "/htmx.min.js", "htmx.min.js");
+    serve_static!(app, "text/css", "/styles.css", "styles.css");
+    app
 }
